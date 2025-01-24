6ae0a02ed96f63279b28a4df84a5089fc1024ed3
EthanF99 <ethanfuks602@gmail.com>
2025-01-24 12:43:46 -0500

    Add my solutions to 5 6 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
+    let bigger = a;
+    let smaller = b;
+    if (b > a){
+        bigger = b;
+        smaller = a;
+    }
+    if (b < 0 || a < 0 || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
+    while(smaller <= bigger){
+        total += smaller;
+        smaller++;
+    }
+    return total;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = parseFloat(((fahrenheit-32)*5/9).toFixed(1));
+  if (celsius % 1 === 0){
+    return Math.floor(celsius);
+  }
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = parseFloat(((celsius*(9/5))+32).toFixed(1));
+  if (fahrenheit % 1 === 0){
+    return Math.floor(fahrenheit);
+  }
+  return fahrenheit;
 };
 
 // Do not edit below this line
