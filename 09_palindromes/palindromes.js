3b9081defe0225e28df047dab6bc71160b261411
EthanF99 <ethanfuks602@gmail.com>
2025-01-28 21:50:01 -0500

    Add solutions for 8 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let regex = new RegExp("[A-z0-9]");
+    //remove whitespace/punctuation then make everything lowercase
+    let forward = input.split("").filter((x) => {return regex.test(x)}).map((x) => x.toLowerCase());
+    let backward = forward.toReversed();
+    //convert to strings for comparison because array comparison comapares references 
+    return forward.join("") === backward.join(""); 
 };
 
 // Do not edit below this line
